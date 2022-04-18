edb02e259fd88f177f02e84dafa0ed3ceb6432d2
haleyalee <haleyalee@gmail.com>
2022-04-18 15:42:52 -0400

    Created searchbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import './Home.css';
-
 import Nav from '../Nav/Nav';
-import Recipe from '../Recipe/Recipe';
 import RecipeCard from '../RecipeCard/RecipeCard';
+import Searchbar from '../Searchbar/Searchbar';
 
 const data = [
   { id: 0, img: '', title: "Recipe Title", ingredients: ['ingr1', 'ingr2', 'ingr3'], instructions: ['step1', 'step2', 'step3'], tags: ['tag', 'tag'] },
@@ -23,6 +22,9 @@ function Home() {
     <>
       <Nav />
       <div className="container py-3">
+
+        <Searchbar />
+
         <Grid container spacing={8}
           id="recipes"
           display="flex"
